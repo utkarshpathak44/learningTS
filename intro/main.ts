7f17781b877c73150f9f748cb69c3a8f7b497a58
utkarshpathak44 <utkarsh.pathak@remotestate.com>
2025-01-29 10:12:13 +0530

    first commit

---
+++ intro/main.ts
@@ -0,0 +1,17 @@
+console.log("i hope this runs")
+
+interface item {
+  itemName: string;
+  itemId: number;
+} 
+
+const questions=[
+    "what is your establishment id",
+    "expected return on your investment",
+    "investments, if any",
+
+]
+
+type answer= undefined | [];
+let data;
+console.log(data)
